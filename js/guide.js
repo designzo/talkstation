6796d5a1b3809b0550210ee4ab69f60d792b760d
designyzo <designyzo@gmail.com>
2026-03-03 16:29:46 +0900

    작업완료

---
+++ js/guide.js
@@ -0,0 +1,26 @@
+$(function () {
+  $('[data-toggle="collapse"]').on('click', function () {
+    var $btn = $(this);
+    var $target = $($btn.data('target'));
+
+    // 같은 collapse를 제어하는 모든 버튼 찾기
+    var targetId = $btn.data('target');
+    var $allBtns = $('[data-toggle="collapse"][data-target="' + targetId + '"]');
+
+    $target.one('shown.bs.collapse', function () {
+      $allBtns.each(function () {
+        $(this).removeClass('collapsed');
+        $(this).find('.guide-toggle-text').text('닫기');
+        $(this).find('.bi').removeClass('bi-chevron-down').addClass('bi-chevron-up');
+      });
+    });
+
+    $target.one('hidden.bs.collapse', function () {
+      $allBtns.each(function () {
+        $(this).addClass('collapsed');
+        $(this).find('.guide-toggle-text').text('자세히 보기');
+        $(this).find('.bi').removeClass('bi-chevron-up').addClass('bi-chevron-down');
+      });
+    });
+  });
+});
